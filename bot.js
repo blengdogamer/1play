4460e722799bb58bb11af3e2879ceafb5c52d720
blengdogamer <40428498+blengdogamer@users.noreply.github.com>
2018-09-06 22:47:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const client = new Discord.Client();
 const { PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
-const prefix = '1';
 const youtube = new YouTube(GOOGLE_API_KEY);
 const queue = new Map();
 
